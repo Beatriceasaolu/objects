69ceecc4dc3cab9a69bf012a9b1c72ff8bd259ab
asaolu james <asaolujtomi@gmail.com>
2023-02-27 23:39:51 +0100

    question five completed

--- index.js
+++ index.js
@@ -32,3 +32,14 @@ object.assign({}, food)
 
 // question five
 //enumeration in javascript
+const presidentialCandidates = {
+    AAC: 'Omoyele Sowore',
+    ACCORD: 'Christopher Imumolen',
+    APC: 'Bola Tinubu Ahmed',
+    LP: 'Peter Obi',
+    NNPP: 'Rabiu Kwankwaso',
+    PDP: 'Atiku Abubakar',
+}
+for (const property in presidentialCandidates){
+    console.log(`${property} is the presidential candiidate of ${object[property]}`);
+}
